2442ada580aa74f132160d9943f078f943892bcc
Yousef Abdelsalam <yousefeid707@gmail.com>
2025-04-29 08:10:47 -0400

    trying out not using these

--- lua/config/core/init.lua
+++ lua/config/core/init.lua
@@ -4,9 +4,9 @@ require("config.core.autocmds")
 
 -- Change the Diagnostic symbols in the sign column (gutter)
 -- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-
-for type, icon in pairs(signs) do
-  local h1 = "DiagnosticSign" .. type
-  vim.fn.sign_define(h1, { text = icon, texthl = h1, numhl = "" })
-end
+-- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+--
+-- for type, icon in pairs(signs) do
+--   local h1 = "DiagnosticSign" .. type
+--   vim.fn.sign_define(h1, { text = icon, texthl = h1, numhl = "" })
+-- end
